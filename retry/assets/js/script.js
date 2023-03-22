acbc3285d0721846257563d364d69a9a9d371d0c
cviguers <cviguers@gmail.com>
2023-03-22 13:17:37 -0400

    click

--- retry/assets/js/script.js
+++ retry/assets/js/script.js
@@ -1,17 +1,31 @@
-
 var searchBar = document.getElementById('searchBar');
 var form = document.querySelector('form');
 var resultDiv = document.getElementById('result');
 
-form.addEventListener('submit', event => {
+var getSearchedHistory = function() {
+    return JSON.parse(localStorage.getItem('searchedPoke'));
+};
+
+var setSearchedHistory = function(text) {
+    var searchedPoke = getSearchedHistory() || [];
+    searchedPoke.push(text);
+    localStorage.setItem('searchedPoke', JSON.stringify(searchedPoke));
+};
+
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
     var searchValue = searchBar.value.toLowerCase();
     fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
-        .then(response => response.json())
-        .then(data => {
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
             var name = data.name;
             var spriteUrl = data.sprites.front_default;
-            var types = data.types.map(type => type.type.name);
+            var types = data.types.map(function(type) {
+                return type.type.name;
+            });
             var resultHTML =  `
             <h2>${name}</h2>
             <img src="${spriteUrl}" alt="${name} sprite">
@@ -19,7 +33,7 @@ form.addEventListener('submit', event => {
           `;
           resultDiv.innerHTML = resultHTML;
         })
-        .catch(error => {
+        .catch(function(error) {
             resultDiv.innerHTML = `<p>${error.message}</p>`;
         });
 });
